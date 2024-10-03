23986c9db84fdec2bbc2f1a0637f5492bf66dff9
TrinidadGabriel <gabrieltc@ciencias.unam.mx>
2024-10-03 11:03:43 -0600

    Update Practica03.hs

--- Practica03.hs
+++ Practica03.hs
@@ -1,1 +1,14 @@
+--Tipo--
+data List a = Void | Node a (List a) deriving Show
 
+--Longitud de una lista.--
+longitud :: List a -> Int
+longitud Void = 0
+longitud (Node a lista) = 1 + longitud lista
+
+---Contencion de un elemento en una lista:---
+estaContenido :: Eq a => List a -> a -> Bool
+estaContenido Void y = False
+estaContenido (Node x xs) elemento= if elemento == x
+                                        then True
+                                        else estaContenido xs elemento
