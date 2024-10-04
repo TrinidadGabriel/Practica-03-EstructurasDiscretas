0262fe77b18fa37c34054b72ac139b2fb4aeaf47
TrinidadGabriel <gabrieltc@ciencias.unam.mx>
2024-10-04 11:59:29 -0600

    Update Practica03.hs

--- Practica03.hs
+++ Practica03.hs
@@ -1,29 +1,36 @@
+--Definicióon de una lista--
 data List a = Void | Node a (List a) deriving Show
 
+--Longitud de una lista.--
 longitud :: List a -> Int
 longitud Void = 0
 longitud (Node a lista) = 1 + longitud lista
 
+--Contención de un elemento en una lista.--
 estaContenido :: Eq a => List a -> a -> Bool
 estaContenido Void y = False
 estaContenido (Node x xs) elemento = if elemento == x 
                                     then True
                                     else estaContenido xs elemento 
 
+--Convertir una lista de haskell a nuestra nueva estructura de lista.--
 convertirAEstructura :: [a] -> List a
 convertirAEstructura [] = Void
 convertirAEstructura (x:xs) = Node x (convertirAEstructura xs)  
 
+--Convertir nuestra nueva estructura de lista a una lista ya definida en haskell--
 convertirALista :: List a -> [a]
 convertirALista Void = []
 convertirALista (Node a lista) = a : convertirALista lista   
 
+--Convertir en un conjunto nuestra nueva estructura de lista.--
 conjunto :: Eq a => List a -> List a
 conjunto Void = Void
 conjunto (Node x lista) = if estaContenido lista x 
                             then conjunto lista
                             else Node x (conjunto lista)
 
+--Eliminar un elemento en un  ́ındice espec ́ıfico.--
 eliminarIndice :: List a -> Int -> List a
 eliminarIndice Void indice = error "No existe indice en la lista vacia"
 eliminarIndice (Node a lista) indice = if indice < 0 || indice > longitud (Node a lista) - 1
@@ -32,6 +39,7 @@ eliminarIndice (Node a lista) indice = if indice < 0 || indice > longitud (Node 
                                             then lista
                                             else Node a (eliminarIndice lista (indice - 1))
 
+--Agregar un elemento en un índice específico.--
 insertarIndice :: List a -> Int -> a -> List a
 insertarIndice Void indice x = if indice /= 0
                                 then error "El indice esta fuera de la lista"
@@ -44,6 +52,7 @@ insertarIndice  (Node a lista) indice x = if indice < 0 || indice > longitud (No
                                             then Node x (Node a lista)
                                             else Node a (insertarIndice lista (indice-1) x) 
 
+--Recorrer n veces a la derecha los elementos de nuestra nueva estructura lista.--
 recorrerLista :: List a -> Int -> List a
 recorrerLista Void recorrido = Void
 recorrerLista lista 0 = lista
