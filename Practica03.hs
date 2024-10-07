88e6de8b676b778003c3496277d482af86aabe16
Alan-Vazquez <vazalan17@ciencias.unam.mx>
2024-10-07 17:59:30 -0600

    Add files via upload

--- Practica03.hs
+++ Practica03.hs
@@ -26,33 +26,29 @@ convertirALista (Node a lista) = a : convertirALista lista
 --Convertir en un conjunto nuestra nueva estructura de lista.--
 conjunto :: Eq a => List a -> List a
 conjunto Void = Void
-conjunto (Node x lista) = if estaContenido lista x 
+conjunto (Node a lista) = if estaContenido lista a 
                             then conjunto lista
-                            else Node x (conjunto lista)
+                            else Node a (conjunto lista)
 
 --Eliminar un elemento en un índice específico.--
 eliminarIndice :: List a -> Int -> List a
 eliminarIndice Void indice = error "No existe indice en la lista vacia"
+eliminarIndice (Node a lista) 0 = lista
 eliminarIndice (Node a lista) indice = if indice < 0 || indice > longitud (Node a lista) - 1
                                         then error "El indice esta fuera de la lista"
-                                        else if indice == 0 
-                                            then lista
-                                            else Node a (eliminarIndice lista (indice - 1))
+                                        else Node a (eliminarIndice lista (indice - 1))
 
 --Agregar un elemento en un índice específico.--
 insertarIndice :: List a -> Int -> a -> List a
 insertarIndice Void indice x = if indice /= 0
                                 then error "El indice esta fuera de la lista"
-                                else if indice == 0 
-                                then (Node x Void)
-                                else error "El indice esta fuera de la lista"
+                                else (Node x Void)
+insertarIndice (Node a lista) 0 x = Node x (Node a lista)
 insertarIndice  (Node a lista) indice x = if indice < 0 || indice > longitud (Node a lista) 
                                             then error "El indice excede la longitud de la lista"
-                                            else if indice == 0
-                                            then Node x (Node a lista)
                                             else Node a (insertarIndice lista (indice-1) x) 
 
---Recorrer n veces a la derecha los elementos de nuestra nueva estructura lista.--
+--Recorrer n veces a la izquierda los elementos de nuestra nueva estructura lista.--
 recorrerLista :: List a -> Int -> List a
 recorrerLista Void recorrido = Void
 recorrerLista lista 0 = lista
